81f5113afc6f5e068713e3335ea49078689e0472
Gabo-1221 <153243524+Gabo-1221@users.noreply.github.com>
2024-08-27 21:57:12 -0600

    subir proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from "./counter/counter.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Angular-app';
+export class AppComponent implements OnInit {
+  title: String = 'Hola mundo angular desde componentes ';
+
+  subtitle = 'Contador con estado de session';
+  users = ['pepe','Maria','Juan', 'Andres'];
+  visivle:Boolean = false;
+
+  counter:number = 0;
+
+  ngOnInit(): void {
+    this.counter = parseInt(sessionStorage.getItem('counter')!) || 0 ;
+  }
+  setVisible(): void {
+    this.visivle = this.visivle ? false : true;
+    console.log('hemos hecho clic en setVisible');
+  }
+  setCounter(counter:number) : void {
+    this.counter = counter
+  }
 }
